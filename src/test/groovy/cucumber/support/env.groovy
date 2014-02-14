a31cfad84470580fed62ca7507e61b002a4d8ea4
STAGR005 <Robert.Stagner@disney.com>
2014-02-14 09:18:27 -0800

    * based on IntelliJ support, updated DSL file

--- src/test/groovy/cucumber/support/env.groovy
+++ src/test/groovy/cucumber/support/env.groovy
@@ -14,11 +14,8 @@ World {
     def cucumberWithGroovyWorld = new CucumberWithGroovyWorld()
     cucumberWithGroovyWorld.metaClass.mixin([MyHelper, TestEnvironment])
     return cucumberWithGroovyWorld
-
-//    new CucumberWithGroovyWorld(myHelper: new MyHelper(), testEnvironment: new TestEnvironment())
 }
 
 Before() {
-//    CucumberWithGroovyWorld world = delegate as CucumberWithGroovyWorld
     println "\nCall ${myTestMethod()} method from ${this.getClass()} BEFORE hook\n"
 }
